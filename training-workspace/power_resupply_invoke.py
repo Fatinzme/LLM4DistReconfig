551aa14e7cac43d79e42a234c3984a64aa848c27
Kiana <poichen@outlook.com>
2025-05-07 21:42:39 +0800

    新增调用模型的代码

--- training-workspace/power_resupply_invoke.py
+++ training-workspace/power_resupply_invoke.py
@@ -26,19 +26,19 @@ def parse_args():
     parser.add_argument('--case_name', type=str, required=True, help='dataset name.')
     parser.add_argument('--model_id', type=str, required=True, help='Model ID to use.')
     parser.add_argument('--output_model', type=str, required=True, help='Output model path.')
-    parser.add_argument('--num_train_epochs', type=int, required=True, help='Number of training epochs.')
-    parser.add_argument('--batch_size', type=int, required=True, help='Batch size per device.')
-    parser.add_argument('--max_new_tokens', type=int, required=True, help='Maximum number of new tokens generated.')
-    # parser.add_argument('--model_name_hf', type=str, required=True, help='Huggingface model name for upload.')
-    # parser.add_argument('--tokenizer_name_hf', type=str, required=True, help='Huggingface tokenizer name for upload.')
-    parser.add_argument('--custom_loss', type=int, required=True, help='For regular loss use 0. For custom loss with 3 penalty terms use 1.')
-    parser.add_argument('--custom_loss_config', type=str, required=True, help='Custom loss configuration.')
-    parser.add_argument('--cycles_loss_scaling_factor', type=float, required=True, help='Scaling factor for cycles loss')
     parser.add_argument('--model_for_generation_path', type=str, required=True, help='Model path to use for generation.')
     return parser.parse_args()
 
 def main():
     args = parse_args()
+
+    train_dataset, train_df = prepare_resupply_data_llama31(
+        args.data_path,args.case_name
+    )
+
+    model, tokenizer = get_model_and_tokenizer(model_id)
+
+    get_memory_allocated_cached()
 
     os.getcwd()
 
